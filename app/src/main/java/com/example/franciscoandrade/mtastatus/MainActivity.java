5b88cd457a6d795491b8dd049cc32764014d5a09
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-02-06 19:59:06 -0500

    database has been setup

--- app/src/main/java/com/example/franciscoandrade/mtastatus/MainActivity.java
+++ app/src/main/java/com/example/franciscoandrade/mtastatus/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.franciscoandrade.mtastatus;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +10,8 @@ import android.view.View;
 
 import com.example.franciscoandrade.mtastatus.Networking.MTA_Service;
 import com.example.franciscoandrade.mtastatus.Networking.RetrofitClient;
+import com.example.franciscoandrade.mtastatus.database.AppDatabase;
+import com.example.franciscoandrade.mtastatus.database.StationsEntity;
 import com.example.franciscoandrade.mtastatus.model.MTA_Stations;
 
 import java.util.ArrayList;
@@ -18,14 +20,15 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final String TAG = "HELP!!";
-    static MTA_Service mtaService;
+    private MTA_Service mtaService;
+    private AppDatabase db;
+    private List<StationsEntity> stationsEntityList;
+
 
 
     @Override
@@ -34,7 +37,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         showToolBar("MTA STATUS", false);
 
+        stationsEntityList = new ArrayList<>();
+
+
         API_Caller();
+
+
+    }
+
+    private void checkIfDBIsUpdated() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<StationsEntity> allStations;
+
+                db = Room.databaseBuilder(getApplicationContext(),
+                        AppDatabase.class, "Stations").build();
+
+                allStations = db.stationsDao().getALL();
+                Log.d(TAG, "size of stations list: " + String.valueOf(allStations.size()));
+                for (StationsEntity s : allStations) {
+                    Log.d(TAG, "Stations have been saved: ID:" + s.getStationID() + " Name: " + s.getStationName());
+                }
+                db.close();
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void showToolBar(String tittle, boolean upButton) {
@@ -60,7 +93,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static void API_Caller(){
+    public void API_Caller(){
+
 
         mtaService = RetrofitClient.getRetrofit("http://mtaapi.herokuapp.com/")
                 .create(MTA_Service.class);
@@ -72,8 +106,14 @@ public class MainActivity extends AppCompatActivity {
                 if(response.isSuccessful()){
                     MTA_Stations mta_stations;
                     mta_stations = response.body();
+                    Log.d(TAG, "onResponse: " + mta_stations.getResult().get(0).getId());
+                    for (MTA_Stations.Results r: mta_stations.getResult()) {
+                        stationsEntityList.add(new StationsEntity(r.getId(),r.getName()));
+                    }
 
+                    updateDatabase();
                     Log.d(TAG, "onResponse: " + mta_stations);
+                    checkIfDBIsUpdated();
                 }
             }
 
@@ -83,5 +123,24 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+
+    private void updateDatabase() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                db = Room.databaseBuilder(getApplicationContext(),
+                        AppDatabase.class, "Stations").build();
+
+                db.stationsDao().insertAll(stationsEntityList.toArray(new StationsEntity[stationsEntityList.size()]));
+                db.close();
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
