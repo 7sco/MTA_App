7e625f5ce4175f84632b2f4be9eb123ef2547fc6
7sco <xaviandrade14@gmail.com>
2018-02-07 21:57:00 -0500

    second adapter added, list not working , error showing db cannot run on thread it does too much work

--- app/src/main/java/com/example/franciscoandrade/mtastatus/MainActivity.java
+++ app/src/main/java/com/example/franciscoandrade/mtastatus/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.franciscoandrade.mtastatus;
 
 import android.arch.persistence.room.Room;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,7 +12,7 @@ import android.view.View;
 
 import com.example.franciscoandrade.mtastatus.Networking.MTA_Service;
 import com.example.franciscoandrade.mtastatus.Networking.RetrofitClient;
-import com.example.franciscoandrade.mtastatus.controller.StationLineAdapter;
+import com.example.franciscoandrade.mtastatus.controller.LinesAdapter;
 import com.example.franciscoandrade.mtastatus.database.AppDatabase;
 import com.example.franciscoandrade.mtastatus.database.StationsEntity;
 import com.example.franciscoandrade.mtastatus.model.MTA_Stations;
@@ -47,36 +46,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         showToolBar("MTA STATUS", false);
         linesRecyclerHolder=(RecyclerView)findViewById(R.id.linesRecyclerHolder);
-//        linesAdapter= new LinesAdapter(this);
-//        linesRecyclerHolder.setAdapter(linesAdapter);
+        linesAdapter= new LinesAdapter(this);
+        linesRecyclerHolder.setAdapter(linesAdapter);
 
-        List<String> stationLines = new ArrayList<>();
-        stationLines.add("1");
-        stationLines.add("2");
-        stationLines.add("3");
-        stationLines.add("4");
-        stationLines.add("5");
-        stationLines.add("6");
-        stationLines.add("7");
-        stationLines.add("A");
-        stationLines.add("C");
-        stationLines.add("E");
-        stationLines.add("B");
-        stationLines.add("D");
-        stationLines.add("F");
-        stationLines.add("M");
-        stationLines.add("J");
-        stationLines.add("Z");
-        stationLines.add("N");
-        stationLines.add("Q");
-        stationLines.add("R");
-        stationLines.add("W");
-        stationLines.add("G");
-        stationLines.add("L");
-        stationLines.add("S");
+//        List<String> stationLines = new ArrayList<>();
+//        stationLines.add("1");
+//        stationLines.add("2");
+//        stationLines.add("3");
+//        stationLines.add("4");
+//        stationLines.add("5");
+//        stationLines.add("6");
+//        stationLines.add("7");
+//        stationLines.add("A");
+//        stationLines.add("C");
+//        stationLines.add("E");
+//        stationLines.add("B");
+//        stationLines.add("D");
+//        stationLines.add("F");
+//        stationLines.add("M");
+//        stationLines.add("J");
+//        stationLines.add("Z");
+//        stationLines.add("N");
+//        stationLines.add("Q");
+//        stationLines.add("R");
+//        stationLines.add("W");
+//        stationLines.add("G");
+//        stationLines.add("L");
+//        stationLines.add("S");
 
-        StationLineAdapter stationLineAdapter = new StationLineAdapter(stationLines, this);
-        linesRecyclerHolder.setAdapter(stationLineAdapter);
+//        StationLineAdapter stationLineAdapter = new StationLineAdapter(stationLines, this);
+//        linesRecyclerHolder.setAdapter(stationLineAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         linesRecyclerHolder.setLayoutManager(linearLayoutManager);
 //        linesRecyclerHolder.setNestedScrollingEnabled(false);
@@ -149,9 +148,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "onResponse: " + mta_stations.getResult().get(0).getId());
                     for (MTA_Stations.Results r : mta_stations.getResult()) {
                         stationsEntityList.add(new StationsEntity(r.getId(), r.getName()));
-
                     }
-
                     updateDatabase();
                     Log.d(TAG, "onResponse: " + mta_stations);
                     checkIfDBIsUpdated();
